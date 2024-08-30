0925a5048c89af1490393e5b8abf50649dd282ac
Virpro18 <sintawidodo032@gmail.com>
2024-08-30 22:12:24 +0700

    PrList  commit

--- src/components/client/ProjectsHeader.tsx
+++ src/components/client/ProjectsHeader.tsx
@@ -2,12 +2,20 @@ import styles from "../../styles/projects_header.module.css"
 interface props {
   title: string;
 }
+import Arrow from "../../../public/arrow-back.svg"
+const arrow  = "<~~"
 
 export default function ProjectHeader(props:props) {
   return (
     <header className={styles.header}>
-      <button>Back</button>
+      
+      <a href="/"><button className={styles['header-back']}>{arrow}Back</button></a>
       <h1>{props.title}</h1>
+      <nav className={styles.features_nav}>
+        <button>Create</button>
+        {/* <button>terus</button> */}
+        <button>AI</button>
+      </nav>
     </header>
   );
 }
