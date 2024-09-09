bac512cb9c34e71b1ec9cc2a3791c84600a07250
Virpro18 <sintawidodo032@gmail.com>
2024-09-09 21:07:52 +0700

    test  commit

--- src/pages/api/post.ts
+++ src/pages/api/post.ts
@@ -1,43 +1,69 @@
+// import type { APIRoute } from "astro";
+
+// export const POST: APIRoute = async ({ request }) => {
+//   try {
+
+//     const data = await request.formData();
+//     console.log(data)
+//     const name = data.get("name");
+//     const email = data.get("email");
+//   const messages = data.get("message");
+//   const prompt = data.get("prompt")
+//   // Validate the data - you'll probably want to do more than this
+//   if (!name || !email || !messages) {
+//     return new Response(
+//       JSON.stringify({
+//         message: "Missing required fields",
+//       }),
+//       { status: 400 }
+//     );
+//   }
+//   // Do something with the data, then return a success response
+//   return new Response(
+//     JSON.stringify({
+//       message: [
+//         {
+//           name,
+//           email,
+//           messages,
+//           prompt,
+//           message: "Thank you for your message!",
+//         }
+//       ]
+//     }),
+//     { status: 200 }
+//   );
+// } catch (error){
+//   return new Response(
+//     JSON.stringify({
+//       message:"error " + error
+//     })
+//   )
+// }
+// };
+
+// src/pages/api/submit.ts
 import type { APIRoute } from "astro";
+import { getGroqChatCompletion } from "../../Ai/Groq";
 
-export const POST: APIRoute = async ({ request }) => {
-  try {
-
-    const data = await request.formData();
+export const POST: APIRoute = async ({ params, request }) => {
+  // const data = params.id
+  const data = await request.formData();
     console.log(data)
-    const name = data.get("name");
-    const email = data.get("email");
-  const messages = data.get("message");
-  const prompt = data.get("prompt")
-  // Validate the data - you'll probably want to do more than this
-  if (!name || !email || !messages) {
-    return new Response(
-      JSON.stringify({
-        message: "Missing required fields",
-      }),
-      { status: 400 }
-    );
-  }
-  // Do something with the data, then return a success response
+    const prompt = data.get("prompt");
+  //   const email = data.get("email");
+  //   const messages = data.get("message");
+  // const data =
   return new Response(
     JSON.stringify({
-      message: [
+      message: 
         {
-          name,
-          email,
-          messages,
-          prompt,
-          message: "Thank you for your message!",
-        }
-      ]
+          message: "test",
+          data: prompt,
+          response: (await getGroqChatCompletion(prompt)).choices[0].message.content,
+        },
     }),
     { status: 200 }
   );
-} catch (error){
-  return new Response(
-    JSON.stringify({
-      message:"error " + error
-    })
-  )
-}
+  // Validate the data - you'll probably want to do more than this
 };
